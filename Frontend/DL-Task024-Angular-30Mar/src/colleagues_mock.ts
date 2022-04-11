5df929d1e1b8b1cbe1d9ac4bbc71f952fb3d5dfd
Namratha R <99namratha@gmail.com>
2022-04-12 00:26:16 +0530

    commit

---
+++ Frontend/DL-Task024-Angular-30Mar/src/colleagues_mock.ts
@@ -0,0 +1,8 @@
+import { Colleague } from "./app/colleagues";
+export const cl: Colleague[]=[
+    {fname:'Namratha',lname:'R'},
+    {fname:'Vagmi',lname:'Shri'},
+    {fname:'Jagdeesh',lname:'R'},
+    {fname:'Shreya',lname:'Rao'},
+    {fname:'Shantanu',lname:'K'}
+];
